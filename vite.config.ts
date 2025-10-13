da7570d3c612c435ae423784c8144d7765c39e1a
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-10-13 10:23:23 -0400

    Set up TypeScript, React, Vite, Vitest, SCSS, ESLint, NPM scripts, directory structure, .gitignore, build and test GitHub workflow, and README
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+export default defineConfig({
+	plugins: [
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler']]
+			}
+		})
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(process.cwd(), "./"),
+			'@public': path.resolve(process.cwd(), "./public"),
+			"@indoors": path.resolve(process.cwd(), "./public/data/indoors"),
+			"@outdoors": path.resolve(process.cwd(), "./public/data/outdoors"),
+			"@metadata": path.resolve(process.cwd(), "./public/data/metadata"),
+			"@src": path.resolve(process.cwd(), "./src"),
+			"@assets": path.resolve(process.cwd(), "./src/assets"),
+			"@components": path.resolve(process.cwd(), "./src/components"),
+			"@features": path.resolve(process.cwd(), "./src/features"),
+			"@hooks": path.resolve(process.cwd(), "./src/hooks"),
+			"@pages": path.resolve(process.cwd(), "./src/pages"),
+			"@services": path.resolve(process.cwd(), "./src/services"),
+			"@styles": path.resolve(process.cwd(), "./src/styles"),
+			"@types": path.resolve(process.cwd(), "./src/types"),
+			"@utils": path.resolve(process.cwd(), "./src/utils"),
+		}
+	}
+});
