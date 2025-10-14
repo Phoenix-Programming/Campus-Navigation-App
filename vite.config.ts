594628e0b5e99a8152657d26eb1f9c4b479cc874
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-10-14 18:54:12 -0400

    GitHub Pages fix & test
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+	base: "/Campus-Navigation-App/",
 	plugins: [
 		react({
 			babel: {
@@ -12,8 +13,8 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			'@': path.resolve(process.cwd(), "./"),
-			'@public': path.resolve(process.cwd(), "./public"),
+			"@": path.resolve(process.cwd(), "./"),
+			"@public": path.resolve(process.cwd(), "./public"),
 			"@indoors": path.resolve(process.cwd(), "./public/data/indoors"),
 			"@outdoors": path.resolve(process.cwd(), "./public/data/outdoors"),
 			"@metadata": path.resolve(process.cwd(), "./public/data/metadata"),
@@ -26,7 +27,7 @@ export default defineConfig({
 			"@services": path.resolve(process.cwd(), "./src/services"),
 			"@styles": path.resolve(process.cwd(), "./src/styles"),
 			"@types": path.resolve(process.cwd(), "./src/types"),
-			"@utils": path.resolve(process.cwd(), "./src/utils"),
+			"@utils": path.resolve(process.cwd(), "./src/utils")
 		}
 	}
 });
