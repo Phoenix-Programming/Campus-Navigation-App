9a6692953268895b18e72208f007e4175c3136d6
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-10-17 14:59:59 -0400

    Trying a new fix with the styles imports
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
 	base: "/Campus-Navigation-App/",
@@ -8,8 +8,8 @@ export default defineConfig({
 		rollupOptions: {
 			output: {
 				assetFileNames: (assetInfo) => {
-					if (assetInfo.names?.some(n => n.endsWith('.css'))) return 'assets/styles.css';
-					return 'assets/[name]-[hash][extname]';
+					if (assetInfo.name?.endsWith(".css")) return "assets/styles.css";
+					return "assets/[name]-[hash][extname]";
 				}
 			}
 		}
@@ -17,7 +17,7 @@ export default defineConfig({
 	plugins: [
 		react({
 			babel: {
-				plugins: [['babel-plugin-react-compiler']]
+				plugins: [["babel-plugin-react-compiler"]]
 			}
 		})
 	],
