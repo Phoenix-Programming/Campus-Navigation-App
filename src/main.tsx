14ccac520f4a8af59ecfc27bf4012b05c329247d
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-10-17 14:51:50 -0400

    Moved styles import to index head
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import 'leaflet/dist/leaflet.css';
-import '@styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
