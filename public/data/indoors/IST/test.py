dc69a4d64538635a6107ac7d001c8ab3cb1824af
vincent <vincent727a@gmail.com>
2025-11-11 12:42:00 -0500

    SVG to JSON Python Script
    Created a Python Script that
    - parses svg files
    - calculates coordinates
    - declares node types
    - generates JSON objects
    - organizes nodes and types
    
    and tested the script with the IST first floor svg by using:
    
    python3 svg_to_graph.py istF1.svg 1

--- public/data/indoors/IST/test.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def svg_to_json_string(public/data/indoors/IST/istF1.svg, public/data/indoors/IST/ISTgraph.json):
-    with open(public/data/indoors/IST/istF1.svg, 'r') as f:
-        svg_content = f.read()
