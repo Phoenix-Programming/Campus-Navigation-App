a3d651c15d76715b6a5d6dab7aad77b6d4c83f9a
EthanKuhhirte <ekuhhirte@gmail.com>
2026-03-01 15:56:06 -0500

    Added graph navigation system to sort for shortest distances and produce parent nodes. Need to automate the return of shortest path.

--- public/Navigation/navigationtest.py
+++ public/Navigation/navigationtest.py
@@ -1,23 +1,12 @@
 # this is a test file for the python script that will provide navigation
+#imports
+
+from heapq import heapify, heappop, heappush
+#these are used for the priority queue
 
 
 # the graph class will be used to represent the generated class, graph itself is a dictionary of dictionaries
 # this graph class itself is more an experimental design as we keep developing it
-
-
-#class Graph:
-#    def __init__(self, graph: dict = {}):
-#        self.graph = graph # graph is a dictionary, can also be passed a graph when it starts up but it defaults to a default empty dict
-#        #not sure if that should be a colon or a =
-#
-#    def add_edge(self, node1, node2, weight = 1):
-#        if node1 not in self.graph: #checks if the node is already added
-#            self.graph[node1] = {} #Creates the node if it isn't already added
-#        self.graph[node1][node2] = weight #add a connection to its neighbor
-#
-#    def shortest_distances(self, source: str):
-#        distances = {node: float("inf") for node in self.graph}
-#        distances[source] = 0 #Sets the source distance to 0
 
 class Graph:
     def __init__(self, graph = dict()):
@@ -30,3 +19,71 @@ class Graph:
                 self.graph[startingNode] = {} #creates the dictionary of connections for that starting node if it didn't already exist
         self.graph[startingNode][endingNode] = distance;
 
+    def shortest_distances(self, sourceNode: str):
+        # suggests that sourcenode is an str
+        distances = {node: float("inf") for node in self.graph} # Creates a distances dictionary to store the distances from source
+        distances[sourceNode] = 0 # sets the source value to 0
+
+        #initialize a priority queue
+        searchQueue = [(0,sourceNode)]
+        heapify(searchQueue)
+        
+        #Create a set to hold visited nodes
+        visited = set()
+
+        while searchQueue: #runs while search queue is not empty
+            current_distance, current_node = heappop(searchQueue) #pops the highest priority, returns a tuple that is split
+            #between current_distance and current_node. This node should be on the one with the min distance
+
+            if current_node in visited:
+                continue #skip already visited nodes
+            
+            visited.add(current_node) #adds it to the visited nodes so we don't revist it
+
+            for adjacent, weight in self.graph[current_node].items():
+                #goes into the inner dictionary and pulls the adjacent nodes and their values
+                calculated_distance = current_distance + weight #distance from source + distance to next node
+
+                if calculated_distance < distances[adjacent]:
+                    #if this new route of reaching the location is shorter than the previously recorded path
+                    distances[adjacent] = calculated_distance #update the known distance to the more efficent path
+                    heappush(searchQueue, (calculated_distance, adjacent)) #adds the new connection and its weight (based on distance from start)
+
+
+        #calculate shortest path
+
+        predecessors = {node: None for node in self.graph} #creates a dictionary list 
+        for node, distance in distances.items():
+            for adjacent, weight in self.graph[node].items():
+                if distances[adjacent] == distance + weight:
+                    predecessors[adjacent] = node
+        return distances,predecessors;
+
+
+#testing code
+
+inputGraph = {'A': {'B': 3, 'C': 3},
+              'B': {'A': 3, 'D': 3.5, 'E': 2.8},
+              'C': {'A': 3, 'E': 2.8, 'F': 3.5},
+              'D': {'B': 3.5, 'E': 3.1, 'G': 10},
+              'E': {'B': 2.8, 'C': 2.8, 'D': 3.1, 'G': 7},
+              'F': {'G': 2.5, 'C': 3.5},
+              'G': {'F': 2.5, 'E': 7, 'D': 10}}
+
+G = Graph(inputGraph)
+
+#G.add_edge("A", "B", 3)
+#G.add_edge("A", "C", 3)
+#G.add_edge("B", "A", 3)
+#G.add_edge("B", "D", 3.5)
+#G.add_edge("B", "E", 2.8)
+
+
+distances, predecessor = G.shortest_distances("B")
+print(distances, "\n")
+
+to_F = distances["F"]
+print(f"The shortest distance from B to F is {to_F}")
+
+print("\n")
+print(predecessor, "\n")
