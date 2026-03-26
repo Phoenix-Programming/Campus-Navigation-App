87e7a9168c82816e2fcd6c20950f9f39434baaff
Jsut a bird <evan.i.chan@gmail.com>
2026-03-26 11:22:02 -0400

    Added script features and example svg and graph
    - added extra checks to prevent duplicate node connection overrides
    - rm nodes will be created if adding a rmdoor node
    - added check_connections and create_missing_nodes in order to ensure both way connections and referenced nodes are created
    - added functionality to create chains of hall nodes for quicker utilization
    - prompts user if they want to create missing nodes and make sure all connections are both ways

--- src/utils/node_connections_helper_script.py
+++ src/utils/node_connections_helper_script.py
@@ -21,7 +21,8 @@ class Node:
         self.connections = []
 
     def add_connection(self, other_node_id):
-        self.connections.append(other_node_id)
+        if other_node_id not in self.connections:
+            self.connections.append(other_node_id)
 
     def to_dict(self):
         return {
@@ -44,8 +45,21 @@ class Graph:
         self.nodes = {}
 
     def add_node(self, node_id):
+        # Create node if it doesn't exist
         if node_id not in self.nodes:
             self.nodes[node_id] = Node(node_id)
+
+        parts = node_id.split("_")
+
+        if parts[1] == "rmdoor": # if the node is an rmdoor, create a corresponding rm node and connect them
+            new_id = f"{parts[0]}_rm_{parts[3]}_{parts[4]}"
+
+            if new_id not in self.nodes:
+                self.nodes[new_id] = Node(new_id)
+                print(f"rm Node {new_id} created for corresponding rmdoor node {node_id}")
+
+            self.nodes[new_id].add_connection(node_id)
+            self.nodes[node_id].add_connection(new_id)
 
     def to_dict(self):
         return {
@@ -61,6 +75,31 @@ class Graph:
 
         return graph
     
+
+# Check all connections and ensure that they are both ways (if node A is connected to node B, then node B should also be connected to node A)
+# if they are not, create the missing connection and print a message indicating that the connection was added
+def check_connections(graph):
+    for node in graph.nodes.values():
+        for connected_node_id in node.connections:
+            if connected_node_id in graph.nodes:
+                connected_node = graph.nodes[connected_node_id]
+                if node.node_id not in connected_node.connections:
+                    connected_node.add_connection(node.node_id)
+                    print(f"Connection added from {connected_node_id} to {node.node_id}")
+
+#Creates nodes for connections that are referenced but not defined in the graph, and prints a message indicating that the node was created
+def create_missing_nodes(graph):
+    missing = set()
+
+    for node in graph.nodes.values():
+        for connected_node_id in node.connections:
+            if connected_node_id not in graph.nodes:
+                missing.add(connected_node_id)
+
+    for node_id in missing:
+        graph.add_node(node_id)
+        print(f"Node {node_id} created because it was referenced in connections but not defined in the graph.")
+
 # Load exisiting graph from json file
 def load_graph(filename):
     with open(filename, "r") as f:
@@ -74,25 +113,57 @@ def save_graph(graph, filename):
 
 # Add base node to graph, then prompt user to add more nodes and connections until they choose to stop
 def add_nodes(graph):
-    print("Nodes will be connected to the base node until you choose to stop adding nodes.\n Input STOP to stop adding nodes.\n")
-    base_node_id = input("Enter the base node id: ")
-    graph.add_node(base_node_id)
-    node_id = ""
+    print("Nodes will be connected to the base node until you choose to stop adding nodes.\n Nodes will be added if referenced, so create all hall chains first, then use rmdoors as base nodes and connect them to the hall nodes, then add any other nodes and connections as needed. rmdoors will automatically be connected to their respective rm node.\n")
     while True:
-        while True:
-            node_id = input("Enter a node id to connect to the base node (or STOP to stop adding nodes): ")
-            if not (node_id == "STOP" or node_id == "stop"):
-                graph.nodes[base_node_id].add_connection(node_id) #open the node with the base node id and add a connection to the new node
+        choice = input("Enter the base node id to connect and add nodes, hall for creating a hall chain, or STOP to stop adding nodes: ")
+        if choice.lower() == "hall": # help create a sequential chain of nodes for a hall
+            print("Example hall node id parameters: \tist_hall_e-w_e_1_f1 \n\t\t ist_hall_e-w are the first three node identifiers for the hall including the underscores \n\t\t e is the last letter of the hall node count identifier \n\t\t 1 is the number for the designation \n\t\t f1 is the suffix for the hall nodes after the count identifier")
+            hall_base = input("Enter the starting three node identifiers for the hall including the underscores (EG: ist_hall_e-w): ")
+            hall_count = (input("Enter the last letter of the hall node count identifier: "))
+            if hall_count.isalpha() and len(hall_count) == 1:
+                hall_count = ord(hall_count) - ord('a') + 1
             else:
-                break
+                print("Invalid input for hall count identifier.")
+                return
+            hall_suffix = input("Enter the suffix for the hall nodes after the count identifier: (EG: 2_f1): ")
 
-        # Once nodes have been added, prompt user to add another base node and repeat the process until they choose to stop
-        base_node_id = input("Enter the base node id: ")
-        if not (base_node_id == "STOP" or base_node_id == "stop"):
-            graph.add_node(base_node_id)
+            for i in range(1, hall_count + 1): # create the chain of nodes for the hall and add connections between them
+                node_id = f"{hall_base}_{chr(ord('a') + i - 1)}_{hall_suffix}"
+                graph.add_node(node_id)
+                print(f"Node {node_id} added to graph.")
+                if i > 1:
+                    graph.nodes[node_id].add_connection(f"{hall_base}_{chr(ord('a') + i - 2)}_{hall_suffix}") # add connection to previous node
+                    graph.nodes[f"{hall_base}_{chr(ord('a') + i - 2)}_{hall_suffix}"].add_connection(node_id) # add reverse connection
+            print(f"Hall chain of {hall_count} nodes created and connected.")
+
+
+        elif not (choice.lower() == "stop"):
+            graph.add_node(choice)
             node_id = ""
+            base_node_id = choice
+            while True:
+                while True:
+                    node_id = input("Enter a node id to connect to %s (or STOP to stop adding connections): " % base_node_id)
+                    if not (node_id.lower() == "stop"):
+                        graph.nodes[base_node_id].add_connection(node_id) #open the node with the base node id and add a connection to the new node
+                    else:
+                        break
+
+                # Once nodes have been added, prompt user to add another base node and repeat the process until they choose to stop
+                base_node_id = input("Enter the base node id: ")
+                if not (base_node_id.lower() == "stop"):
+                    graph.add_node(base_node_id)
+                    node_id = ""
+                else:
+                    break
         else:
             break
+    #once out of the loop, ask to perform a check to create missing nodes that are referenced, and make sure all connections are both ways
+    if input("Do you want to check for missing nodes and connections? (y/n): ").lower() in ["y", "yes"]:
+        create_missing_nodes(graph)
+        check_connections(graph)
+    
+
 
 
 # main loop
