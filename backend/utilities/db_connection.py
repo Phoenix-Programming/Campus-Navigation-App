252a14d8b2121b66861fde2ef5b2c6ab2c994bd9
Daniel Freer <deletes_minimal.0w@icloud.com>
2026-06-25 00:43:38 -0400

    corrected alembic migrations
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- backend/utilities/db_connection.py
+++ backend/utilities/db_connection.py
@@ -1,8 +1,18 @@
 from fastapi import Depends
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 from typing import Annotated
 from backend.settings import settings
+
+
+POSTGRES_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
 
 engine: AsyncEngine = create_async_engine(settings.db_url)
@@ -15,7 +25,7 @@ AsyncSessionLocal = async_sessionmaker(
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=POSTGRES_CONVENTION)
 
 
 async def get_db():
