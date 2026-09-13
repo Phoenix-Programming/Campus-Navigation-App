f07ce97f38ddaefb084549690405966163010b8d
Daniel Freer <deletes_minimal.0w@icloud.com>
2026-09-13 17:09:02 -0400

    fixed configuration issue in vite config
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from '@rolldown/plugin-babel'
 import path from "path";
 
 export default defineConfig({
 	plugins: [
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]]
-			}
-		})
+		react(),
+		babel({
+			presets: [reactCompilerPreset()],
+		}),
 	],
 	resolve: {
 		alias: {
