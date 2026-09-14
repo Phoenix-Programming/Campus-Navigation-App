6f56e92df9415862a849d9c6eaec191528fc366b
Daniel Freer <deletes_minimal.0w@icloud.com>
2026-09-14 18:04:36 -0400

    created unauthorized page
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

---
+++ frontend/src/pages/Unauthorized.tsx
@@ -0,0 +1,9 @@
+export default function Unauthorized() {
+	return (
+		<div>
+			<h1>Unauthorized</h1>
+			<p>You are not authorized to access the requested page.</p>
+			<a href="/">Return to Home</a>
+		</div>
+	);
+}
