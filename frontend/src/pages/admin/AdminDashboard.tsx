c6bbce0e853c46b40a26086dc07b39bd3e08b941
Daniel Freer <deletes_minimal.0w@icloud.com>
2026-09-24 22:36:42 -0400

    admin dashboard
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- frontend/src/pages/admin/AdminDashboard.tsx
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -11,6 +11,8 @@ export default function RootLayout() {
 			<Link to="/admin/account">Account</Link>
 			<br />
 			<Link to="/admin/node-connections-editor">Node Connections Editor</Link>
+			<br />
+			<Link to="/admin/indoor-map-editor">Indoor Map Editor</Link>
 		</>
 	);
 }
